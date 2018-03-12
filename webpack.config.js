afd03a6dd344c4b11c1ca7b6fd847fd327f2d624
shzshz7 <shzshz777@gmail.com>
2018-03-11 23:24:02 -0400

    to correct some errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //entry -> output   
 
 const path = require('path');
@@ -40,46 +39,3 @@ module.exports = {
 
 
 
-=======
-//entry -> output   
-
-const path = require('path');
-
-// console.log(__dirname);
-// console.log(path.join(__dirname, 'public'));
-
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module:{
-        rules:[{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },{
-            test:/\.s?css$/,
-            use:[
-                'style-loader', 
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
-    }
-};
-
-
-
-//loader
-
-
-
-
-
->>>>>>> ea5a41ba284f67198f0c124faf826847d5cb5247
